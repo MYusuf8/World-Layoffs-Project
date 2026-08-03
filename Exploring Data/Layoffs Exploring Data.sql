cb431a71e432fcee1652aa361e22ae3fe89a69a2
MYusuf8 <myusuf898@outlook.com>
2026-08-03 22:40:29 +0100

    Add files via upload

---
+++ Exploring Data/Layoffs Exploring Data.sql
@@ -0,0 +1,87 @@
+# Exploring the Data
+
+SELECT *
+FROM layoffs2;
+
+SELECT MAX(total_laid_off), MAX(percentage_laid_off)
+FROM layoffs2;
+
+SELECT *
+FROM layoffs2
+WHERE percentage_laid_off = 1;
+
+SELECT company, SUM(total_laid_off)
+FROM layoffs2
+GROUP BY company
+ORDER BY 2 DESC;
+
+SELECT MIN(`date`), MAX(`date`)
+FROM layoffs2;
+
+SELECT industry, SUM(total_laid_off)
+FROM layoffs2
+GROUP BY industry
+ORDER BY 2 DESC;
+
+SELECT country, SUM(total_laid_off)
+FROM layoffs2
+GROUP BY country
+ORDER BY 2 DESC;
+
+SELECT YEAR(`date`), SUM(total_laid_off)
+FROM layoffs2
+GROUP BY YEAR(`date`)
+ORDER BY 1 DESC;
+
+SELECT SUBSTRING(`date` ,1,7) AS `MONTH`, SUM(total_laid_off)
+FROM layoffs2
+WHERE SUBSTRING(`date` ,1,7) IS NOT NULL
+GROUP BY `MONTH`
+ORDER BY 1 ASC;
+
+WITH Rolling_Total AS 
+(
+SELECT SUBSTRING(`date` ,1,7) AS `MONTH`, SUM(total_laid_off) AS Total_off
+FROM layoffs2
+WHERE SUBSTRING(`date` ,1,7) IS NOT NULL
+GROUP BY `MONTH`
+ORDER BY 1 ASC
+)
+SELECT `MONTH`, Total_off
+,SUM(total_off) OVER(ORDER BY `MONTH`) AS rolling_total
+FROM Rolling_Total;
+
+SELECT company,YEAR(`date`), SUM(total_laid_off)
+FROM layoffs2
+GROUP BY company, YEAR(`date`)
+ORDER BY 3 DESC;
+
+WITH Company_Year (company, years, total_laid_off) AS 
+(
+SELECT company,YEAR(`date`), SUM(total_laid_off)
+FROM layoffs2
+GROUP BY company, YEAR(`date`)
+ORDER BY 3 DESC
+), company_year_rank AS
+
+(
+SELECT *, 
+DENSE_RANK() OVER (PARTITION BY years ORDER BY total_laid_off DESC) AS Ranking
+FROM Company_Year
+WHERE years IS NOT NULL
+)
+SELECT *
+FROM company_year_rank
+WHERE Ranking <= 5
+;
+
+SELECT *
+FROM layoffs2;
+
+
+
+
+
+
+
+
